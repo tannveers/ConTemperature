ccc52b620212f2022578fd74d74503ad8b269237
tannveers <tanveer9812@gmail.com>
2020-06-30 18:43:41 +0530

    Updates

---
+++ precache-manifest.b66db5c9956d5759222123268fcab080.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76355c0775703710751f24612ed006ef",
+    "url": "/ConTemperature/index.html"
+  },
+  {
+    "revision": "87ae3884e954f9039ccb",
+    "url": "/ConTemperature/static/css/main.8d61f437.chunk.css"
+  },
+  {
+    "revision": "37aab77a636f24d314e9",
+    "url": "/ConTemperature/static/js/2.3fc81135.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ConTemperature/static/js/2.3fc81135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87ae3884e954f9039ccb",
+    "url": "/ConTemperature/static/js/main.a64770ab.chunk.js"
+  },
+  {
+    "revision": "33c4aff30c263ca20857",
+    "url": "/ConTemperature/static/js/runtime-main.19235a4b.js"
+  },
+  {
+    "revision": "fe8abcfb9e41b7b45108c32fd5160b3e",
+    "url": "/ConTemperature/static/media/cold.fe8abcfb.jpg"
+  },
+  {
+    "revision": "dad33e70a8c03dc90ed0dbb7881439a6",
+    "url": "/ConTemperature/static/media/orange.dad33e70.jpg"
+  },
+  {
+    "revision": "87e169db50a54bffddb5b650d9b13d1f",
+    "url": "/ConTemperature/static/media/weather.87e169db.png"
+  }
+]);
